428fdb533e991f40af2512bb73fee366311950d5
Sven den Boer <denboer.sven@gmail.com>
2019-03-25 10:57:59 +0100

    fixed classes

--- app/src/main/java/com/example/bp3/models/Inschrijving.java
+++ app/src/main/java/com/example/bp3/models/Inschrijving.java
@@ -3,6 +3,10 @@ package com.example.bp3.models;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * @author Sven
+ */
+
 @Setter
 @Getter
 public class Inschrijving {
--- app/src/main/java/com/example/bp3/models/Opdracht.java
+++ app/src/main/java/com/example/bp3/models/Opdracht.java
@@ -1,15 +1,21 @@
 package com.example.bp3.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * @author Sven
+ */
+
 @Getter
 @Builder
+@AllArgsConstructor
 public class Opdracht extends Activiteit {
 
     private int leerjaar;
     private String opdrachtNaam, lesvak, eisen;
-    private final Docent docent;
+    private Docent docent;
 
     public Opdracht() {
     }
