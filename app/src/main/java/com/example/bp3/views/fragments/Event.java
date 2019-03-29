3237ea9e2a93078af0e9f9cd6328af63ab9e15db
Koen Franken <47524445+Koen1904@users.noreply.github.com>
2019-03-29 14:12:15 +0100

    save

--- app/src/main/java/com/example/bp3/views/fragments/Event.java
+++ app/src/main/java/com/example/bp3/views/fragments/Event.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,6 @@ import com.example.bp3.R;
 import com.example.bp3.views.fragments.ViewFragment;
 
 public class Event extends ViewFragment {
-
 
     @Override
     protected int layoutFragment() {
@@ -24,4 +24,8 @@ public class Event extends ViewFragment {
     protected int title() {
         return R.string.events_event;
     }
+
+
+
+
 }
